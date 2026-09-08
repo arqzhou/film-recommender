94d1725ace83a6282edd28fca7b9eab3c734dac6
arqzhou <arqzhou@gmail.com>
2026-09-08 00:37:17 -0500

    Inserted both tables, just trying to figure out movies with the most 5-star ratings

--- script.py
+++ script.py
@@ -10,8 +10,8 @@ print("Path to dataset files:", path)
 
 movies = pd.read_csv(f"{path}/movies.csv")
 ratings = pd.read_csv(f"{path}/ratings.csv")
-# print(movies.head())
-# print(ratings.head())
+#print(movies.head())
+print(ratings.head())
 
 
 def make_db():
@@ -37,6 +37,45 @@ def make_db():
             FOREIGN KEY (movieId) REFERENCES movies(movieId)
         ON DELETE CASCADE
     );""")
+    #cur.executemany("INSERT INTO trips (trip_id, start_date, feed_timestamp, schedule_relationship, route_id, direction, vehicle_id) VALUES (?, ?, ?, ?, ?, ?, ?)", trips_data)
+    print(movies.shape)
+    cur.executemany("INSERT INTO movies (movieId, title, genres) VALUES (?, ?, ?)", movies.values.tolist())
+    cur.executemany("INSERT INTO ratings (userId, movieId, rating, timestamp) VALUES (?, ?, ?, ?)", ratings.values.tolist())
+
+    cur.execute("""
+        SELECT
+            movieId,
+            COUNT(*) AS movie_count
+        FROM ratings
+        WHERE rating = 5
+        GROUP BY movieId
+        HAVING movie_count > 100
+        ORDER BY movie_count DESC;
+""")
+    rows = cur.fetchall()
+    top_movies = pd.DataFrame(columns=['movieId', 'rating_num', 'movieName'])
+    for row in rows:
+        
+        top_movies.loc[len(top_movies)] = [row[0], row[1], "Unknown"]
+        # print(row)
+    print(top_movies.head())
+
+    top_movie_ids = top_movies['movieId'].tolist()
+    print(top_movie_ids)
+    print({','.join(['?']*len(top_movie_ids))})
+
+
+    cur.execute(f"""
+        SELECT
+            title
+        FROM movies
+        WHERE movieId IN ({','.join(['?']*len(top_movie_ids))})
+    """)
+
+    second_rows = cur.fetchall()
+    for second_row in second_rows:
+        print(row)
+
     con.commit()
     con.close()
     
