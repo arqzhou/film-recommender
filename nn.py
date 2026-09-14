8356e9aa0931e7f421cbdf2a4709d7d00a7fda87
arqzhou <arqzhou@gmail.com>
2026-09-14 12:35:11 -0500

    Created Y_norm matrix in cost_function(Y,R):

--- nn.py
+++ nn.py
@@ -1,54 +1,54 @@
 import sqlite3
-from bidict import bidict
 import numpy as np
+import tensorflow as tf
 
 con = sqlite3.connect("movielens.db")
 cur = con.cursor()
 
 
 # Finds all the unique user IDs in the dataset and keeps them in users.
-cur.execute("""
-    SELECT r.userId
-    FROM ratings r
-    GROUP BY r.userId
-    ;
-""")
+# cur.execute("""
+#     SELECT r.userId
+#     FROM ratings r
+#     GROUP BY r.userId
+#     ;
+# """)
 
-users = cur.fetchall()
-print(users[:10])
+# users = cur.fetchall()
+# print(users[:10])
 
 # Finds all movies that a parameterized user(s) has not rated.
-unrated_query = """
-    SELECT m.*, r.userId, r.movieId
-    FROM movies m
-    LEFT JOIN ratings r
-        ON m.movieId = r.movieId AND r.userId = ?
-    WHERE r.movieId IS NULL
-    ;
-"""
-# Printing for sanity check
-for user in users[:3]:
-        cur.execute(unrated_query, user)
-        unrated_rows = cur.fetchall()
-        for unrated_row in unrated_rows[:10]:
-            print(unrated_row)
+# unrated_query = """
+#     SELECT m.*, r.userId, r.movieId
+#     FROM movies m
+#     LEFT JOIN ratings r
+#         ON m.movieId = r.movieId AND r.userId = ?
+#     WHERE r.movieId IS NULL
+#     ;
+# """
+# # Printing for sanity check
+# for user in users[:3]:
+#         cur.execute(unrated_query, user)
+#         unrated_rows = cur.fetchall()
+#         for unrated_row in unrated_rows[:10]:
+#             print(unrated_row)
 
-# Create popularity baseline if a new user hasn't rated anything.
-baseline_query = """
-    SELECT m.movieId, m.title, i.avg_rating, i.num_ratings
-    FROM movies m
-    INNER JOIN (
-        SELECT movieId, AVG(rating) as avg_rating, COUNT(*) as num_ratings
-        FROM ratings
-        GROUP BY movieId
-        HAVING num_ratings > 10
-        ORDER BY avg_rating DESC) i
-    ON m.movieId = i.movieId
-    ;
-"""
-cur.execute(baseline_query)
-avg_ratings = cur.fetchall()
-print(avg_ratings[:10])
+# # Create popularity baseline if a new user hasn't rated anything.
+# baseline_query = """
+#     SELECT m.movieId, m.title, i.avg_rating, i.num_ratings
+#     FROM movies m
+#     INNER JOIN (
+#         SELECT movieId, AVG(rating) as avg_rating, COUNT(*) as num_ratings
+#         FROM ratings
+#         GROUP BY movieId
+#         HAVING num_ratings > 10
+#         ORDER BY avg_rating DESC) i
+#     ON m.movieId = i.movieId
+#     ;
+# """
+# cur.execute(baseline_query)
+# avg_ratings = cur.fetchall()
+# print(avg_ratings[:10])
 
 # Checks the amount of movies that would be kept based on rating threshold
 # percentage_query = """
@@ -133,4 +133,21 @@ def make_matrices():
     print(r_matrix[:5, :5])
     print(b_matrix[:5, :5])
 
-make_matrices()
+    return(r_matrix, b_matrix)
+
+def cost_function(Y, R):
+    print(len(Y), len(Y[1]))
+    X = tf.Variable(tf.random.normal((len(Y),25), dtype=tf.float64), name = "movie_features")
+    W = tf.Variable(tf.random.normal((len(Y[0]),25), dtype=tf.float64), name = "user_features")
+    b = tf.Variable(tf.random.normal((len(Y[0]),), dtype=tf.float64), name = "user_bias")
+
+    epsilon = .00001
+    mean_i = np.sum(Y * R, axis = 1) / (np.sum(R, axis = 1) + epsilon) # avoids the divide by 0 error
+    Y_norm = Y - mean_i[:, None]
+    print(Y_norm[:5, :5])
+
+    preds = tf.linalg.matmul(X, W, transpose_b = True) + b
+
+
+
+cost_function(*make_matrices())
